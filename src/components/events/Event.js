8076085fe3591a717f1e85639d08b0813173a9a8
dayanalorza <dayanalorza@gmail.com>
2019-07-16 10:09:10 -0700

    Converted Event to functional component added useEffect hook

--- src/components/events/Event.js
+++ src/components/events/Event.js
@@ -1,72 +1,59 @@
-import React, { Fragment, Component } from "react";
+import React, { Fragment, useEffect } from "react";
 import Spinner from "../layout/Spinner";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 
-export class Event extends Component {
-  componentDidMount() {
-    // console.log(this.props.match.params.id);
-    this.props.getEvent(this.props.match.params.id);
-  }
+const Event = ({ event, loading, getEvent, match }) => {
+  useEffect(() => {
+    getEvent(match.params.id);
+    //mimic componentDidMount with empty brackets[]
+    //eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-    loading: PropTypes.bool,
-    event: PropTypes.object.isRequired,
-    getEvent: PropTypes.func.isRequired
-  };
-  render() {
-    const {
-      logo,
-      url,
-      id,
-      name,
-      locale,
-      is_free,
-      description
-    } = this.props.event;
-    const { loading } = this.props;
+  const { logo, url, id, name, locale, is_free, description } = event;
 
-    console.log(this.props.event);
-    if (loading) return <Spinner />;
+  // console.log(this.props.event);
+  if (loading) return <Spinner />;
+  return (
+    <Fragment>
+      {name && <h2 style={{ marginBottom: "40px" }}>{name.text}</h2>}
+      <Link to="/" className="btn btn-dark">
+        Back
+      </Link>
+      Free:{" "}
+      {is_free ? (
+        <i className="fas fa-check text-success" />
+      ) : (
+        <i className="fas fa-times-circle text-danger" />
+      )}
+      <div className="grid-2">
+        <div className="card">
+          {logo && (
+            <img src={logo.original.url} alt="" style={{ width: "460px" }} />
+          )}
 
-    return (
-      <Fragment>
-        {name && <h2 style={{ marginBottom: "40px" }}>{name.text}</h2>}
-        <Link to="/" className="btn btn-dark">
-          Back
-        </Link>
-        Free:{" "}
-        {is_free ? (
-          <i className="fas fa-check text-success" />
-        ) : (
-          <i className="fas fa-times-circle text-danger" />
-        )}
+          {url && (
+            <a
+              href={url}
+              target="_blank"
+              className="btn btn-dark"
+              rel="noopener noreferrer"
+            >
+              Tickets
+            </a>
+          )}
+        </div>
         <div className="grid-2">
-          <div className="card">
-            {logo && (
-              <img src={logo.original.url} alt="" style={{ width: "460px" }} />
-            )}
-
-            {url && (
-              <a
-                href={url}
-                target="_blank"
-                className="btn btn-dark"
-                rel="noopener noreferrer"
-              >
-                Tickets
-              </a>
-            )}
-          </div>
-          <div className="grid-2">
-            {description && (
-              <p style={{ width: "400px" }}>{description.text}</p>
-            )}
-          </div>
-        </div>{" "}
-      </Fragment>
-    );
-  }
-}
+          {description && <p style={{ width: "400px" }}>{description.text}</p>}
+        </div>
+      </div>{" "}
+    </Fragment>
+  );
+};
+Event.propTypes = {
+  loading: PropTypes.bool,
+  event: PropTypes.object.isRequired,
+  getEvent: PropTypes.func.isRequired
+};
 
 export default Event;
